b0db570480d5dc45a0fcd74c5e67ecdad8d7639f
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-01-09 14:13:20 -0500

    create about page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default () => (
+  <div className="about">
+    <h3>Let me tell you a story, all about how</h3>
+    <p>my react got flipped, turned upside down</p>
+    <img
+      src="https://source.unsplash.com/random/150x150"
+      alt="about unsplash"
+    />
+  </div>
+);
