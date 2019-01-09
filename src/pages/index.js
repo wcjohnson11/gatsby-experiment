360f506c2c0772f70fb0735be068afa13e7c74e1
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-01-09 14:21:01 -0500

    refactor index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from "react"
+import Header from '../components/header'
 
 export default () => (
-    <div className="hero">
-        <h1>Gatsby Experiment</h1>
-        <p>What a time to be alive!</p>
-        <img src="https://source.unsplash.com/random/900x200" alt="random unsplash" />
+    <div>
+        <Header />
+        <p>Living in the future</p>
     </div>
 )
