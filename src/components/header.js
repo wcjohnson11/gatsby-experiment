d9c41e6739592a943e7f04f20371e89bc7d2c443
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-01-09 16:38:52 -0500

    implement css modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
+import headerStyles from "./header.module.css"
 
 export default (props) => (
-  <div className="hero">
+  <div className={headerStyles.header}>
     <Link to={props.routeLink}>{props.routeName}</Link>
     <h1>Gatsby Experiment</h1>
     <p>What a time to be alive!</p>
