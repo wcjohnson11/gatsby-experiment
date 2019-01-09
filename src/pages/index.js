1f6c2974d3ce876abe39915450b368bbfe6230e7
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-01-09 14:03:22 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default () => (
+    <div className="hero">
+        <h1>Gatsby Experiment</h1>
+        <p>What a time to be alive!</p>
+        <img src="https://source.unsplash.com/random/900x200" alt="random unsplash" />
+    </div>
+)
