3db077ef7fdb3687162c17bf38af30eb0c0972b1
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-01-09 19:50:58 -0800

    refactor header

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
-import headerStyles from "./header.module.css"
+import ListLink from "./listLink";
+import styles from "./header.module.css"
 
 export default () => (
-  <div className={headerStyles.header}>
-    <Link to="/">Home</Link>
-    <Link to="/about/">About</Link>
-    <Link to="/contact/">Contact</Link>
-    <h1>Gatsby Experiment</h1>
+  <div className={styles.header}>
+    <Link to="/" className={styles.headerTitle}>
+      <h1>Gatsby Experiment</h1>
+    </Link>
+    <ul className={styles.headerList}>
+      <ListLink to="/">Home</ListLink>
+      <ListLink to="/about/">About</ListLink>
+      <ListLink to="/contact/">Contact</ListLink>
+    </ul>
     <p>What a time to be alive!</p>
     <img
       src="https://source.unsplash.com/random/800x200"
