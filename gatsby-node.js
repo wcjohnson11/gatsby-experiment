2fe653cb428389205b2d0c171a6f357877632f8b
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-01-11 14:09:36 -0800

    programmatically create blog posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -15,20 +16,33 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 exports.createPages = ({ graphql, actions}) => {
      // **Note:** The graphql function call returns a Promise 
      // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise for more info
+    const { createPage } = actions
     return graphql(`
-        {
-            allMarkdownRemark {
-                edges {
-                    node {
-                        fields {
-                            slug
-                        }
-                    }
-                }
-            }
-        }
+       {
+           allMarkdownRemark {
+               edges {
+                   node {
+                       fields {
+                           slug
+                       }
+                   }
+               }
+           }
+       }
     `
     ).then(result => {
-        console.log(JSON.stringify(result, null, 4))
+        result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+            createPage({
+              path: node.fields.slug,
+              component: path.resolve(
+                `./src/templates/blog-post.js`
+              ),
+              context: {
+                // Data passed to context is available
+                // in page queries as GraphQL variables.
+                slug: node.fields.slug,
+              },
+            })
+        })
     })
 }
