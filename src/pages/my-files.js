6efe6f9a702df273038b11324ebae11dc14709bc
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-01-11 14:39:44 -0800

    add slug links to blog posts

--- src/pages/my-files.js
+++ src/pages/my-files.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 
 export default ({ data }) => {
@@ -30,11 +30,13 @@ export default ({ data }) => {
           <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
           {data.allMarkdownRemark.edges.map(({ node }) => (
             <div key={node.id}>
-              <h3>
-                {node.frontmatter.title}{" "}
-                <span> - {node.frontmatter.date}</span>
-              </h3>
-              <p>{node.excerpt}</p>
+                <Link to={node.fields.slug}>
+                    <h3>
+                    {node.frontmatter.title}{" "}
+                    <span> - {node.frontmatter.date}</span>
+                    </h3>
+                    <p>{node.excerpt}</p>
+                </Link>
             </div>
           ))}
         </div>
@@ -62,6 +64,9 @@ export const query = graphql`
                     frontmatter {
                         title
                         date(formatString: "DD MMMM, YYYY")
+                    }
+                    fields {
+                        slug
                     }
                     excerpt
                 }
