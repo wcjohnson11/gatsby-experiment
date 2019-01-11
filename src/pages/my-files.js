c021ab674ca69ac5b99f33a23dc7c1bdeb23b214
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-01-11 14:03:51 -0800

    add posts to my-files pages

--- src/pages/my-files.js
+++ src/pages/my-files.js
@@ -3,50 +3,47 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
 export default ({ data }) => {
-    console.log(data)
-    return (
-        <Layout>
-            <div>
-                <h1>My site's files</h1>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>absolutePath</th>
-                            <th>relativePath</th>
-                            <th>prettySize</th>
-                            <th>extension</th>
-                            <th>birthTime</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.allFile.edges.map(({ node }, index) => (
-                            <tr key={index}>
-                                <td>{node.absolutePath}</td>
-                                <td>{node.relativePath}</td>
-                                <td>{node.prettySize}</td>
-                                <td>{node.extension}</td>
-                                <td>{node.birthTime}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-                <h1>My site's posts</h1>
-                <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
-                {data.allMarkdownRemark.edges.map(({node}) => (
-                    <div key={node.id}>
-                        <h3>{node.frontmatter.title}{" "}</h3>
-                        <span> - {node.frontmatter.date}</span>
-                        <p>{node.excerpt}</p>
-                    </div>
-                ))}
+    return <Layout>
+        <div>
+          <h1>My site's files</h1>
+          <table>
+            <thead>
+              <tr>
+                <th>absolutePath</th>
+                <th>relativePath</th>
+                <th>prettySize</th>
+                <th>extension</th>
+                <th>birthTime</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.allFile.edges.map(({ node }, index) => <tr key={index}>
+                  <td>{node.absolutePath}</td>
+                  <td>{node.relativePath}</td>
+                  <td>{node.prettySize}</td>
+                  <td>{node.extension}</td>
+                  <td>{node.birthTime}</td>
+                </tr>)}
+            </tbody>
+          </table>
+          <h1>My site's posts</h1>
+          <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+          {data.allMarkdownRemark.edges.map(({ node }) => (
+            <div key={node.id}>
+              <h3>
+                {node.frontmatter.title}{" "}
+                <span> - {node.frontmatter.date}</span>
+              </h3>
+              <p>{node.excerpt}</p>
             </div>
-        </Layout>
-    )
+          ))}
+        </div>
+      </Layout>;
 }
 
 export const query = graphql`
     query {
-        allFile {
+        allFile(sort: { fields: [birthtime], order: DESC}) {
             edges {
                 node {
                     relativePath
@@ -57,7 +54,7 @@ export const query = graphql`
                 }
             }
         }
-        allMarkdownRemark {
+        allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC}) {
             totalCount
             edges {
                 node {
