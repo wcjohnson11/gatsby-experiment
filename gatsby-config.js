c96d93ad9fd032b3c7f2200a4c6be60f5985a531
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-01-11 10:59:48 -0800

    fix gatsby-config source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
             },
         },
         {
-            resolve: 'gatsby-plugin-filesystem',
+            resolve: 'gatsby-source-filesystem',
             options: {
                 name: 'src',
                 path: `${__dirname}/src/`
