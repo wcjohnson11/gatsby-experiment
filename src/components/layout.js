b34fa222e86f668530bba5defbc38999ec8ccb0a
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-01-29 20:47:57 -0500

    update container column width

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,12 @@
 import React from "react"
 import Container from "./container"
 import Header from "./header"
+import "./pure.css"
 
 export default ({ children }) => (
     <Container>
         <Header />
-        <h1>hi</h1>
+        <h1 className="pure-u-1-24">hi</h1>
         {children}
     </Container>
 )
