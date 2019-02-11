4c5162ab78e559f4e4b3c8624c9b25190fb2595f
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-02-11 16:43:20 -0500

    update legend component and presentation

--- src/components/visualizations/legend.js
+++ src/components/visualizations/legend.js
@@ -5,11 +5,30 @@ import style from './legend.module.css';
 class Legend extends React.Component {
 	render() {
 		const { scale, legendClick } = this.props;
+		const Labels = scale.domain().map((label, i) => {
+			const size = 15;
+			return (
+				<LegendItem
+					key={`legend-quantile-${i}`}
+					margin="0 5px"
+					alignItems="center"
+					onClick={() => legendClick(label)}
+				>
+					<svg width={size} height={size}>
+						<circle fill={label.value} r={size / 2} cx={size / 2} cy={size / 2} />
+					</svg>
+					<LegendLabel align={'left'}>
+						{label.text}
+					</LegendLabel>
+				</LegendItem>
+			)
+		})
+		console.log(Labels)
 		return (
 			<div className={`pure-u-1 ${style.legend}`}>
 				<div className={style.title}>Continents</div>
 				<LegendOrdinal scale={scale} labelFormat={(label) => `${label.toUpperCase()}`}>
-					{(labels) => {
+					{(labels, i) => {
 						return (
 							<div className={style.row}>
 								{labels.map((label, i) => {
