ebd64ea965d646a9e644cf0c1684f9af8cb1b227
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-02-11 20:04:12 -0500

    add text and formatting

--- src/pages/happiness.js
+++ src/pages/happiness.js
@@ -139,7 +139,7 @@ class Happiness extends React.Component {
 				return result;
 			}, []);
 			Seda.x = 'GDP per Capita';
-			Seda.y = 'sustainable economic development assessment (SEDA)';
+			Seda.y = 'SEDA';
 
 			const EconomicFreedom = happy.reduce((result, d) => {
 				if (d['overall economic freedom score'] !== 'FALSE' && d['GDP per capita (PPP)']) {
@@ -225,48 +225,60 @@ class Happiness extends React.Component {
 
 		return (
 			<Layout>
-				<h1 className={style.title}>Measuring Happiness Around The World</h1>
-				<p>What does it mean to live a good life? For most people, the answer will be happiness. Other popular answers, such as love, money, freedom, security or community could arguably be considered the building blocks or prerequisites for happiness. If there is a strong corrollation between happiness and quality of life, then if we can find a way to model happiness in a society, then we can measure it's quality of life, observe changes over time, refine our models and maximize for policies that lead to more happiness.</p>
+				<h1 className={style.title}>Measuring Wellbeing for a Better World</h1>
+				<h3>The history of measuring human wellbeing</h3>
+				<p>The history of attempting to measure human wellbeing on a global scale is still relatively new. After World War 2 and the Great Depression, when countries were attempting to find ways to measure their recoveries, economist Simon Kuznets developed the concept of a Gross Domestic Product (GDP) to capture all economic production by individuals, companies and the government in a single statistic. He theorized that it should correlate to wellbeing since it would rise in good times and fall in bad.</p>
+				<p>This was easily measurable and served as the primary measure of a country's wellbeing for many years but over time, criticism arose that growth in a country's economic output doesn't necessarily mean a better life for it's citizens.</p>
+				<p>In 1990, UN economist Mahbub ul Haq created the Human Development Index (HDI) to more accurately measure human wellbeing, which focused on education, health expectancy, gender equality, and standard of living. In 2012, UN Secretary General Ban Ki Moon recognized the importance of making happiness a goal of public policy.</p>
 				<h3>What is Happiness anyway?</h3>
 				<blockquote>
 					<p>Happiness is the experience of joy, contentment, or positive well-being, combined with a sense that one’s life is good, meaningful, and worthwhile.</p>
 					<footer>Psychologist Sonja Lyubomirsky in her book <cite><a href="https://www.amazon.com/gp/product/0143114956">The How of Happiness</a></cite></footer>
 				</blockquote>
-				<p>I like this definition of happiness, it clearly seperates happiness into two main parts, positive emotions such as joy, contentment, interest and love and then the sense of satisfaction that comes with achieving life goals. The emotional aspect is universal while the second is more variable depending on a person's values. Different cultures and life experiences will lead to different values. For instance, in a survey that 
-					. It's an entirely different question, although very interesting, if we can view what is happiness as a view into what people feel like is missing from their lives, or what gives them purpose. For interest, when Sonja Lyubomirsky did a research project asking people in America and Russia to define this, she received very different answers. American people said money, family, success, having fun, Russian people said spiritual salvation, a world of peace and beauty, mutual understanding among people. It's interesting to think about what </p>
-				<p>Happines is cultural (america > money, family, success, having fun,,,, spiritual salvation, a world of peace and beauty, mutual understanding among people</p>
+				<p>This definition clearly seperates happiness into two main parts, the first, happiness as positive emotions such as joy, contentment, interest and love and the second, as an evaluation of human wellbeing, a sense of fulfillment and satisfaction that comes from living a good life.</p>
+				<p>The emotional aspect is universal while the second depends on a person's culture and life experiences. For example, when Sonja Lyubomirsky did a research project asking people in America and Russia to define happiness,American people said money, family, success, having fun while Russian people said spiritual salvation, a world of peace and beauty, mutual understanding among people.</p>
+				<p>Happiness is difficult to measure but if there is a strong corrollation between happiness and quality of life, then if we can find a way to model happiness in a society, then we can measure it's quality of life, observe changes over time, refine our models and maximize for policies that lead to more happiness and avoid policies that lead to less happiness.</p>
+				<h3>Different Ways of Measuring Wellbeing</h3>
+				<p>There have been different attempts at measuring wellbeing and they will be described below. By visualizing these metrics of wellbeing alongside the GDP per capita, we can see that there is a rough correlation between increased GDP per capita and improved scores on these wellbeing metrics.</p>
+				{isPromiseResolved && (
+					<div className="pure-g">
+						<Legend scale={zScale} legendClick={this.handleLegendClick} />
+						<div className="pure-u-1 pure-u-md-1-5">
+							<VxScatterplotWithSize data={happyPlanet} currentContinent={currentContinent} zScale={zScale} useGrid={false} />
+						</div>
+						<div className="pure-u-1 pure-u-md-1-5">
+							<VxScatterplotWithSize data={humanDevIndex} currentContinent={currentContinent} zScale={zScale} useGrid={false} />
+						</div>
+						<div className="pure-u-1 pure-u-md-1-5">
+							<VxScatterplotWithSize data={Seda} currentContinent={currentContinent} zScale={zScale} useGrid={false} />
+						</div>
+						<div className="pure-u-1 pure-u-md-1-5">
+							<VxScatterplotWithSize data={happiness} currentContinent={currentContinent} zScale={zScale} useGrid={false} />
+						</div>
+						<div className="pure-u-1 pure-u-md-1-5">
+							<VxScatterplotWithSize data={economicFreedom} currentContinent={currentContinent} zScale={zScale} useGrid={false} />
+						</div>
+					</div>
+				)}
+				The below metrics are structured in such a way that lower values are better. A similar correlation with GDP per capita can be seen for these metrics.
+				{ isPromiseResolved && (
+					<div className="pure-g">
+						<div className="pure-u-1 pure-u-md-1-5">
+							<VxScatterplotWithSize data={gini} currentContinent={currentContinent} zScale={zScale} useGrid={false} />
+						</div>
+						<div className="pure-u-1 pure-u-md-1-5">
+							<VxScatterplotWithSize data={civilLiberties} currentContinent={currentContinent} zScale={zScale} useGrid={false} />
+						</div>
+						<div className="pure-u-1 pure-u-md-1-5">
+							<VxScatterplotWithSize data={politicalRights} currentContinent={currentContinent} zScale={zScale} useGrid={false} />
+						</div>
+					</div>
+				)}
+				<h4>GINI index</h4>
+				<p>One of the oldest measurements of wellbeing is The GINI index, developed by Italian Statistician Corrado Gini in 1912, measures inequality in income distribution in family income, with 0 representing perfect equality (everyone earns exactly the same) and 1 representing perfect inequality (one family earns everything, everyone else earns nothing). The GINI index is an imperfect measurement because it focuses on income, rather than wealth, which is much harder to measure because of unreliable GDP and income data. Shadow economies and tax havens make it hard to measure income and wealth, particularly in developing countries. Despite this, it appears to be fairly well correlated to GDP per capita, meaning that countries with a higher GDP per capita tend to have less inequality, at least as far as it can be measured.</p>
 				<VariableForm handleFieldSelect={this.handleVariableFieldSelect} variableValue={variableValue} />
 				{isPromiseResolved && (
 					<React.Fragment>
-						<div className="pure-g">
-							<Legend scale={zScale} legendClick={this.handleLegendClick} />
-							<div className="pure-u-1 pure-u-md-1-5">
-								<VxScatterplotWithSize data={happyPlanet} currentContinent={currentContinent} zScale={zScale} useGrid={false} />
-							</div>
-							<div className="pure-u-1 pure-u-md-1-5">
-								<VxScatterplotWithSize data={humanDevIndex} currentContinent={currentContinent} zScale={zScale} useGrid={false} />
-							</div>
-							<div className="pure-u-1 pure-u-md-1-5">
-								<VxScatterplotWithSize data={Seda} currentContinent={currentContinent} zScale={zScale} useGrid={false} />
-							</div>
-							<div className="pure-u-1 pure-u-md-1-5">
-								<VxScatterplotWithSize data={happiness} currentContinent={currentContinent} zScale={zScale} useGrid={false} />
-							</div>
-							<div className="pure-u-1 pure-u-md-1-5">
-								<VxScatterplotWithSize data={economicFreedom} currentContinent={currentContinent} zScale={zScale} useGrid={false} />
-							</div>
-						</div>
-						<div className="pure-g">
-							<div className="pure-u-1 pure-u-md-1-5">
-								<VxScatterplotWithSize data={gini} zScale={zScale} useGrid={false} />
-							</div>
-							<div className="pure-u-1 pure-u-md-1-5">
-								<VxScatterplotWithSize data={civilLiberties} zScale={zScale} useGrid={false} />
-							</div>
-							<div className="pure-u-1 pure-u-md-1-5">
-								<VxScatterplotWithSize data={politicalRights} zScale={zScale} useGrid={false} />
-							</div>
-						</div>
 						<div className="pure-g">
 							<div className="pure-u-1 pure-u-md-1-5">
 								<VxBarChart data={happyPlanet} zScale={zScale} />
