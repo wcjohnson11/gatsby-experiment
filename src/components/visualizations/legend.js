9c97b63e6d3fae1d8f9cfbe2353be3c45c85647a
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-02-11 21:23:52 -0500

    update legend

--- src/components/visualizations/legend.js
+++ src/components/visualizations/legend.js
@@ -5,25 +5,6 @@ import style from './legend.module.css';
 class Legend extends React.Component {
 	render() {
 		const { scale, legendClick } = this.props;
-		const Labels = scale.domain().map((label, i) => {
-			const size = 15;
-			return (
-				<LegendItem
-					key={`legend-quantile-${i}`}
-					margin="0 5px"
-					alignItems="center"
-					onClick={() => legendClick(label)}
-				>
-					<svg width={size} height={size}>
-						<circle fill={label.value} r={size / 2} cx={size / 2} cy={size / 2} />
-					</svg>
-					<LegendLabel align={'left'}>
-						{label.text}
-					</LegendLabel>
-				</LegendItem>
-			)
-		})
-		console.log(Labels)
 		return (
 			<div className={`pure-u-1 ${style.legend}`}>
 				<div className={style.title}>Continents</div>
@@ -36,16 +17,14 @@ class Legend extends React.Component {
 									return (
 										<LegendItem
 											key={`legend-quantile-${i}`}
-                                            margin="0 5px"
-                                            alignItems="center"
+											margin="0 5px"
+											alignItems="center"
 											onClick={() => legendClick(label)}
 										>
-											<svg width={size} height={size}>
+											<svg width={size} height={size} style={{ marginRight: 5 }}>
 												<circle fill={label.value} r={size / 2} cx={size / 2} cy={size / 2} />
 											</svg>
-											<LegendLabel align={'left'}>
-												{label.text}
-											</LegendLabel>
+											<LegendLabel align={'left'}>{label.text}</LegendLabel>
 										</LegendItem>
 									);
 								})}
