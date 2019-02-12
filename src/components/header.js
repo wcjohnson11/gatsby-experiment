f1122e00e4689d7e66bafa9b097a577a3eaab9d6
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-02-11 19:18:58 -0500

    remove comments from header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { graphql, Link, StaticQuery } from "gatsby"
-import ListLink from "./listLink";
 import styles from "./header.module.css"
 
 export default () => (
@@ -18,14 +17,8 @@ export default () => (
   render={data => (
 
     <div className={styles.header}>
-    <Link to="/" className={styles.headerTitle}>
-      <a>{data.site.siteMetadata.title}</a>
-    </Link>
-    {/* <ul className={styles.headerList}>
-      <ListLink to="/about/">About</ListLink>
-      <ListLink to="/contact/">Contact</ListLink>
-    </ul> */}
-  </div>
+      <Link to="/" className={styles.headerTitle}>{data.site.siteMetadata.title}</Link>
+    </div>
   )}
   />
 );
