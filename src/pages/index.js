4cf4e6f556eb8f50a9e303326d0c88d7e5828bd8
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-02-19 18:33:42 -0500

    update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,50 @@
-import React from "react"
-import Layout from "../components/layout"
+import React from "react";
+import Layout from "../components/layout";
+import { select } from "d3";
+import style from "./index.module.css";
+class index extends React.Component {
+  componentDidMount() {
+    select("svg")
+      .append("circle")
+      .attr("cx", 150)
+      .attr("cy", 150)
+      .attr("r", 120)
+      .style("fill", "#592037")
+      .classed(`${style.circle}`, true);
 
-export default () => (
-    <Layout>
-        <p>Living in the future, with React!</p>
-        <div className="pure-g">
-            <p className="pure-u-12-24">Left Column</p>
-            <p className="pure-u-12-24">Right Column</p>
+    select("svg")
+      .append("text")
+      .attr("x", 115)
+      .attr("y", 150)
+      .attr("dy", ".35em")
+      .text(() => "Welcome :)");
+
+    // Orange FF530D
+    // Red E82C0C
+    // Purple 33004B
+    // Blue 03B0F0
+    // Pink FF0DFF
+  }
+  render() {
+    return (
+      <Layout>
+        <div className={style.svg}>
+          <svg width={300} height={300} />
         </div>
-    </Layout>
-)
+        <div className={style.title}>
+          <h1>Will Johnson</h1>
+          <p>Analytics, Art, Data, Engineering</p>
+        </div>
+        <div className={style.contentColumns}>
+          <div className={`pure-u-1 pure-u-md-12-24 ${style.projects}`}>
+            <h3>Projects</h3>
+          </div>
+          <div className={`pure-u-1 pure-u-md-12-24 ${style.blog}`}>
+            <h3>Blog</h3>
+          </div>
+        </div>
+      </Layout>
+    );
+  }
+}
+export default index;
