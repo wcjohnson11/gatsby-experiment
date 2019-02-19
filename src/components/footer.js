a3d480ed20a4dc3bc004a0ba82ac496ede8a6a9a
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-02-19 15:18:11 -0500

    update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { FaGithubAlt, FaLinkedinIn, FaCodepen } from "react-icons/fa";
+import { MdMailOutline } from "react-icons/md";
 import style from "./footer.module.css";
 
 export default () => (
   <div className={style.footer}>
-    <p>Hello and Thanks!</p>
+    <FaGithubAlt className={style.icon} />
+    <FaLinkedinIn className={style.icon} />
+    <FaCodepen className={style.icon} />
+    <MdMailOutline className={style.icon} />
   </div>
 );
