568fb8d030ef4f1bf4f78f07fe711d726c072adf
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-02-19 18:31:45 -0500

    update typography theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,6 @@
-import Typography from "typography"
+import Typography from "typography";
+import GithubTheme from "typography-theme-github";
 
-const typography = new Typography({ baseFontSize: "16px" })
+const typography = new Typography(GithubTheme);
 
-export default typography
+export default typography;
