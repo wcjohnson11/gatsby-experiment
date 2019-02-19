3e5bbb8a6bb9e6b657d0f734ed54ebe56b6a4116
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-02-19 15:44:27 -0500

    add links to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,9 +5,17 @@ import style from "./footer.module.css";
 
 export default () => (
   <div className={style.footer}>
-    <FaGithubAlt className={style.icon} />
-    <FaLinkedinIn className={style.icon} />
-    <FaCodepen className={style.icon} />
-    <MdMailOutline className={style.icon} />
+    <a className={style.a} href="https://www.github.com/wcjohnson11">
+      <FaGithubAlt className={style.icon} />
+    </a>
+    <a className={style.a} href="https://www.linkedin.com/in/wcj11">
+      <FaLinkedinIn className={style.icon} />
+    </a>
+    <a className={style.a} href="https://codepen.io/wcjohnson11/#">
+      <FaCodepen className={style.icon} />
+    </a>
+    <a className={style.a} href="mailto:will@wcj.io">
+      <MdMailOutline className={style.icon} />
+    </a>
   </div>
 );
