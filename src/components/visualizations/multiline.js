8c764d6fbecfbebe10a327abedcd70297cf6339f
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-02-21 20:32:50 -0500

    add labels to multiline

--- src/components/visualizations/multiline.js
+++ src/components/visualizations/multiline.js
@@ -2,6 +2,8 @@ import React from "react";
 import {
   axisBottom,
   axisLeft,
+  curveCatmullRom,
+  event,
   extent,
   line,
   nest,
@@ -40,7 +42,7 @@ class MultiLine extends React.Component {
     const { data, parentWidth } = nextProps;
     if (!data) return {};
 
-    const height = parentWidth * 0.8;
+    const height = parentWidth * 0.6;
     const width = parentWidth;
 
     const filteredData = data.filter(
@@ -49,7 +51,7 @@ class MultiLine extends React.Component {
 
     const xScale = scaleTime()
       .domain(extent(filteredData, d => d.Year))
-      .range([margin.left, width - margin.right]);
+      .range([margin.left, width - margin.right * 4]);
 
     const yScale = scaleLinear()
       .domain(extent(filteredData, d => d["GDP per capita"]))
@@ -57,6 +59,7 @@ class MultiLine extends React.Component {
       .range([height - margin.bottom, margin.top]);
 
     const lineFn = line()
+      .curve(curveCatmullRom)
       .x(d => xScale(d.Year))
       .y(d => yScale(d["GDP per capita"]));
 
@@ -76,7 +79,7 @@ class MultiLine extends React.Component {
   }
 
   componentDidUpdate() {
-    const { lineFn, nestedData, xScale, yScale } = this.state;
+    const { height, lineFn, nestedData, width, xScale, yScale } = this.state;
 
     this.xAxis.scale(xScale);
     select(this.mulitilineXRef.current)
@@ -95,26 +98,78 @@ class MultiLine extends React.Component {
       .attr("dy", ".35em")
       .attr("font-weight", "bold");
 
-    const pathsG = select(".multiLineChart")
+    const focus = select("#multiLine")
       .append("g")
+      .attr("class", "focus")
+      .style("display", "none");
+
+    focus
+      .append("line")
+      .attr("class", "focus line")
+      .attr("y1", 0)
+      .attr("y2", height);
+
+    const handleMouseMove = () => {
+      console.log("why");
+      const xPos = event.pageX;
+      const yPos = event.pageY;
+    };
+
+    const country = select("#multiLine")
+      .selectAll(".country")
+      .data(nestedData)
+      .enter()
+      .append("g")
+      .attr("class", "country")
       .attr("fill", "none")
       .attr("stroke", "lightgray")
       .attr("stroke-width", 1.5)
       .attr("stroke-linejoin", "round")
       .attr("stroke-linecap", "round");
-    nestedData.forEach((country, i) => {
-      pathsG
-        .append("path")
-        .datum(country.values)
-        .style("mix-blend-mode", "multiply")
-        .attr("d", lineFn);
-    });
+
+    country
+      .append("path")
+      .attr("class", "line")
+      .style("mix-blend-mode", "multiply")
+      .attr("d", d => lineFn(d.values))
+      .on("mouseover", () => {
+        console.log("hm");
+        focus.style("display", null);
+      })
+      .on("mouseout", () =>
+        focus
+          .transition()
+          .delay(700)
+          .style("display", "none")
+      )
+      .on("mousemove", handleMouseMove);
+
+    country
+      .append("text")
+      .datum(d => {
+        return {
+          name: d.key,
+          value: d.values[d.values.length - 1]
+        };
+      })
+      .attr("transform", d => {
+        const yValue = yScale(d.value["GDP per capita"]);
+        const xValue = xScale(d.value.Year);
+        return `translate(${xValue}, ${yValue})`;
+      })
+      .attr("x", 3)
+      .attr("dy", ".35em")
+      .attr("font-size", "12px")
+      .attr("stroke", "black")
+      .attr("font-weight", "normal")
+      .style("font-weight", "normal")
+      .text(d => d.name);
   }
 
   render() {
     const { height, width } = this.state;
     return (
-      <svg className="multiLineChart" height={height} width={width}>
+      <svg id="multiLine" height={height} width={width}>
         <g
           ref={this.mulitilineXRef}
           transform={`translate(0, ${height - margin.bottom})`}
