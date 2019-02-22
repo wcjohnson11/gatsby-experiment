e9116c795c33776bafa7ccb6a0c7a64eb8ce8f89
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-02-22 00:53:50 -0500

    d3 multiline hover function

--- src/components/visualizations/multiline.js
+++ src/components/visualizations/multiline.js
@@ -2,8 +2,8 @@ import React from "react";
 import {
   axisBottom,
   axisLeft,
+  bisectLeft,
   curveCatmullRom,
-  event,
   extent,
   line,
   nest,
@@ -11,6 +11,7 @@ import {
   scaleLinear,
   scaleTime
 } from "d3";
+import { event as currentEvent } from "d3-selection";
 import { withParentSize } from "@vx/responsive";
 
 const margin = { top: 20, right: 20, bottom: 30, left: 40 };
@@ -26,6 +27,9 @@ class MultiLine extends React.Component {
         "England",
         "Australia",
         "Japan",
+        "India",
+        "Germany",
+        "Switzerland",
         "China",
         "Brazil"
       ]
@@ -36,6 +40,73 @@ class MultiLine extends React.Component {
 
   xAxis = axisBottom().tickSizeOuter(0);
   yAxis = axisLeft().tickSizeOuter(0);
+
+  hover(svg, path, xScale, yScale, nestedData) {
+    const dateRange = nestedData[0].values.map(d => d.Year);
+    svg.style("position", "relative");
+
+    if ("ontouchstart" in document) {
+      svg
+        .style("-webkit-tap-highlight-color", "transparent")
+        .on("touchmove", moved)
+        .on("touchstart", entered)
+        .on("touchend", left);
+    } else {
+      svg
+        .on("mousemove", moved)
+        .on("mousenter", entered)
+        .on("mouseleave", left);
+    }
+
+    const dot = svg.append("g").attr("display", "none");
+
+    dot.append("circle").attr("r", 2.5);
+
+    dot
+      .append("text")
+      .style("font", "10px sans-serif")
+      .attr("text-anchor", "middle")
+      .attr("y", -8);
+
+    function moved() {
+      currentEvent.preventDefault();
+      const ym = yScale.invert(currentEvent.layerY);
+      const xm = xScale.invert(currentEvent.layerX);
+      const i1 = bisectLeft(dateRange, xm, 1);
+      const i0 = i1 - 1;
+      const i = xm - dateRange[i0] > dateRange[i1] - xm ? i1 : i0;
+
+      const s = nestedData.reduce((a, b) => {
+        return Math.abs(a.values[i]["GDP per capita"] - ym) <
+          Math.abs(b.values[i]["GDP per capita"] - ym)
+          ? a
+          : b;
+      });
+
+      path
+        .attr("stroke", d => (d === s ? "steelblue" : "#ddd"))
+        .filter(d => d === s)
+        .raise();
+
+      dot.attr(
+        "transform",
+        `translate(${xScale(dateRange[i])},${yScale(
+          s.values[i]["GDP per capita"]
+        )})`
+      );
+      dot.select("text").text(s.name);
+    }
+
+    function entered() {
+      path.style("mix-blend-mode", "multiply").attr("stroke", "#ddd");
+      dot.attr("display", null);
+    }
+
+    function left() {
+      path.style("mix-blend-mode", "multiply").attr("stroke", "steelblue");
+      dot.attr("display", "none");
+    }
+  }
 
   static getDerivedStateFromProps(nextProps, prevState) {
     const { activeCountries } = prevState;
@@ -79,7 +150,7 @@ class MultiLine extends React.Component {
   }
 
   componentDidUpdate() {
-    const { height, lineFn, nestedData, width, xScale, yScale } = this.state;
+    const { lineFn, nestedData, xScale, yScale } = this.state;
 
     this.xAxis.scale(xScale);
     select(this.mulitilineXRef.current)
@@ -98,23 +169,6 @@ class MultiLine extends React.Component {
       .attr("dy", ".35em")
       .attr("font-weight", "bold");
 
-    const focus = select("#multiLine")
-      .append("g")
-      .attr("class", "focus")
-      .style("display", "none");
-
-    focus
-      .append("line")
-      .attr("class", "focus line")
-      .attr("y1", 0)
-      .attr("y2", height);
-
-    const handleMouseMove = () => {
-      console.log("why");
-      const xPos = event.pageX;
-      const yPos = event.pageY;
-    };
-
     const country = select("#multiLine")
       .selectAll(".country")
       .data(nestedData)
@@ -122,7 +176,7 @@ class MultiLine extends React.Component {
       .append("g")
       .attr("class", "country")
       .attr("fill", "none")
-      .attr("stroke", "lightgray")
+      .attr("stroke", "steelblue")
       .attr("stroke-width", 1.5)
       .attr("stroke-linejoin", "round")
       .attr("stroke-linecap", "round");
@@ -131,18 +185,7 @@ class MultiLine extends React.Component {
       .append("path")
       .attr("class", "line")
       .style("mix-blend-mode", "multiply")
-      .attr("d", d => lineFn(d.values))
-      .on("mouseover", () => {
-        console.log("hm");
-        focus.style("display", null);
-      })
-      .on("mouseout", () =>
-        focus
-          .transition()
-          .delay(700)
-          .style("display", "none")
-      )
-      .on("mousemove", handleMouseMove);
+      .attr("d", d => lineFn(d.values));
 
     country
       .append("text")
@@ -159,11 +202,14 @@ class MultiLine extends React.Component {
       })
       .attr("x", 3)
       .attr("dy", ".35em")
-      .attr("font-size", "12px")
-      .attr("stroke", "black")
-      .attr("font-weight", "normal")
+      .attr("fill", "black")
+      .attr("stroke-width", 0)
+      .style("font-size", "10px")
+      .style("font-style", "sans-serif")
       .style("font-weight", "normal")
       .text(d => d.name);
+
+    select("#multiLine").call(this.hover, country, xScale, yScale, nestedData);
   }
 
   render() {
