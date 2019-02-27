aeb7cd856b364acac7f1d6f9939253e9a41c8f00
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-02-27 15:41:37 -0500

    update layout styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,11 +2,12 @@ import React from "react";
 import Container from "./container";
 import Header from "./header";
 import Footer from "./footer";
+import style from "./layout.module.css";
 
 export default ({ children }) => (
   <Container>
     <Header />
-    <div style={{ flexGrow: 1 }}>{children}</div>
+    <div className={style.layout}>{children}</div>
     <Footer />
   </Container>
 );
