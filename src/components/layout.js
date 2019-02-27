205a66c791ffb0f4409f85bcd37aeb64e969b97e
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-02-27 14:15:14 -0500

    remove pure.css

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,6 @@ import React from "react";
 import Container from "./container";
 import Header from "./header";
 import Footer from "./footer";
-import "./pure.css";
 
 export default ({ children }) => (
   <Container>
