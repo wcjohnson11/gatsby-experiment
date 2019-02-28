10047152d8ca378cfd811df46c677686edf481b0
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-02-28 02:36:41 -0500

    update layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,12 +2,11 @@ import React from "react";
 import Container from "./container";
 import Header from "./header";
 import Footer from "./footer";
-import style from "./styles/layout.module.css";
 
 export default ({ children }) => (
   <Container>
     <Header />
-    <div className={style.layout}>{children}</div>
+      {children}
     <Footer />
   </Container>
 );
