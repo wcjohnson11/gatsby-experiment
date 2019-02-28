6ccfa36c8709e3b11ab06b672808d03825703d18
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-02-28 00:32:50 -0500

    update heatmap

--- src/components/visualizations/heatmap.js
+++ src/components/visualizations/heatmap.js
@@ -4,7 +4,7 @@ import { axisLeft, axisTop, select, scaleBand } from "d3";
 
 const height = 400;
 const width = 400;
-const margin = { top: 160, left: 175 };
+const margin = { top: 100, left: 125 };
 
 class Heatmap extends React.Component {
   constructor(props) {
@@ -18,7 +18,7 @@ class Heatmap extends React.Component {
   yAxis = axisLeft().tickSizeOuter(0);
 
   static getDerivedStateFromProps(nextProps, prevState) {
-    const { data, columns } = nextProps;
+    const { columns, data } = nextProps;
     if (!data) return {};
 
     const xValues = [
@@ -30,22 +30,22 @@ class Heatmap extends React.Component {
     ];
 
     const yValues = [
-      "GINI Index",
       "Happy Planet Index",
       "Human Development Index",
       "Sustainable Economic Development Index",
+      "GINI Index",
       "World Happiness Report Score"
     ];
 
     const xScale = scaleBand()
       .domain(xValues)
       .range([0, width - margin.left])
-      .padding(0.2);
+      .padding(0.1);
 
     const yScale = scaleBand()
       .domain(yValues)
       .range([0, height - margin.top])
-      .padding(0.2);
+      .padding(0.1);
 
     const rects = columns
       .filter(d => yValues.indexOf(d.name) >= 0)
@@ -68,12 +68,13 @@ class Heatmap extends React.Component {
         return result;
       }, []);
 
-    return { data, columns, rects, xScale, yScale };
+    return { data, rects, xScale, yScale };
   }
 
   componentDidUpdate() {
     const { rects, xScale, yScale } = this.state;
 
+    // Apply xScale and set text value
     this.xAxis.scale(xScale);
     select(this.xAxisRef.current)
       .call(this.xAxis)
@@ -84,6 +85,8 @@ class Heatmap extends React.Component {
       .attr("transform", "rotate(-45)")
       .style("text-anchor", "start")
       .style("font-size", "16px");
+    
+    // Apply yScale and set text value
     this.yAxis.scale(yScale);
     select(this.yAxisRef.current)
       .call(this.yAxis)
@@ -92,51 +95,53 @@ class Heatmap extends React.Component {
       .attr("dy", ".35em")
       .style("font-size", "16px");
 
-    select("#heatmap")
-      .selectAll("rect")
-      .data(rects)
-      .enter()
-      .append("rect")
-      .attr("x", d => d.x)
-      .attr("y", d => d.y)
-      .attr("width", d => d.width)
-      .attr("height", d => d.height)
-      .attr("fill", d => d.fill);
-
-    // add X gridlines
-    select("#heatmap")
+      
+      // add X gridlines
+      select("#heatmap")
       .append("g")
       .attr("class", "grid")
       .attr("transform", `translate(0, ${height - margin.top})`)
       .call(
         axisTop(xScale)
-          .ticks(xScale.domain().length)
-          .tickSize(height - margin.top)
-          .tickFormat("")
-      );
-
-    // add Y Gridlines
-    select("#heatmap")
-      .append("g")
-      .attr("class", "grid")
-      .attr("transform", `translate(${width - margin.left}, 0)`)
-      .call(
-        axisLeft(yScale)
+        .ticks(xScale.domain().length)
+        .tickSize(height - margin.top)
+        .tickFormat("")
+        );
+        
+        // add Y Gridlines
+        select("#heatmap")
+        .append("g")
+        .attr("class", "grid")
+        .attr("transform", `translate(${width - margin.left}, 0)`)
+        .call(
+          axisLeft(yScale)
           .ticks(yScale.domain().length)
           .tickSize(width - margin.left)
           .tickFormat("")
-      );
-  }
-
-  render() {
-    return (
-      <svg
-        id="heatmap"
-        viewBox={`-${margin.left} -${margin.top} ${width +
-          margin.left} ${height + margin.top}`}
-        height={height}
-        width={width}
-      >
+          );
+          
+          // Mount rects and set attr
+          select("#heatmap")
+            .selectAll("rect")
+            .data(rects)
+            .enter()
+            .append("rect")
+            .attr("x", d => d.x)
+            .attr("y", d => d.y)
+            .attr("width", d => d.width)
+            .attr("height", d => d.height)
+            .attr("fill", d => d.fill);
+        }
+        
+        render() {
+          return (
+        <svg
+          id="heatmap"
+          viewBox={`-${margin.left} -${margin.top} ${width +
+            margin.left} ${height + margin.top}`}
+          height={height}
+          width={width}
+        >
         <g ref={this.xAxisRef} />
         <g ref={this.yAxisRef} />
       </svg>
