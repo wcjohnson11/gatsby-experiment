186c0d67cd6b4761d55c8df2e3252f102be891e5
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-02 05:32:16 -0500

    add Mystery Box component

---
+++ src/components/mysterybox.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import style from './styles/mysterybox.module.css';
+
+export default ({}) => (
+    <div className={style.container}>
+        <div className={style.cube}>
+            <div className={`${style.side} ${style.front}`}></div>
+            <div className={`${style.side} ${style.back}`}></div>
+            <div className={`${style.side} ${style.right}`}></div>
+            <div className={`${style.side} ${style.left}`}></div>
+            <div className={`${style.side} ${style.top}`}></div>
+            <div className={`${style.side} ${style.bottom}`}></div>
+            <div className={`${style.questionmark}`}></div>
+        </div>
+    </div>
+)
