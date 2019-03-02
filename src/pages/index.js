6d466042fc7567933d43a13677279755617c6204
wcjohnson11 <williamcouperjohnson@gmail.com>
2019-03-02 05:31:37 -0500

    update home page styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,19 @@
 import React from "react";
 import Layout from "../components/layout";
-import { select } from "d3";
+import Circles from "../components/visualizations/circles";
+import MysteryBox from "../components/mysterybox";
 import style from "./styles/index.module.css";
-class index extends React.Component {
-  componentDidMount() {
-    select("svg")
-      .append("circle")
-      .attr("cx", 150)
-      .attr("cy", 150)
-      .attr("r", 120)
-      .classed(`${style.circle}`, true);
 
-    select("svg")
-      .append("text")
-      .attr("x", 115)
-      .attr("y", 150)
-      .attr("dy", ".35em")
-      .text(() => "Welcome :)");
+class index extends React.Component {
+
+  
+  componentDidMount() {
   }
   render() {
     return (
       <Layout>
         <section className={style.hero}>
-          <div className={style.svg}>
-            <svg width={300} height={300} />
-          </div>
+          <Circles />
           <div className={style.title}>
             <h1>Will Johnson</h1>
             <p>Analytics, Art, Data, Engineering</p>
@@ -32,6 +21,7 @@ class index extends React.Component {
         </section>
         <section className={style.projects}>
           <h3>Projects</h3>
+          <MysteryBox />
         </section>
         <section className={style.blog}>
           <h3>Blog</h3>
